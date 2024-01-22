3716811a0482302e13835d7d4de64644d981e41b
marwa <marwaserrar2002@gmail.com>
2024-01-22 11:40:24 +0100

    second version

---
+++ src/main/java/com/example/school/Controllers/CategorieServiceController.java
@@ -0,0 +1,44 @@
+package com.example.school.Controllers;
+
+import com.example.school.Entities.CategorieService;
+import com.example.school.Entities.Eleve;
+import com.example.school.Repositories.CategorieServiceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/categorieService")
+public class CategorieServiceController {
+    @Autowired
+    private CategorieServiceRepository categorieServiceRepository;
+    @GetMapping
+    public List<CategorieService> getCategorieServices(){
+        return categorieServiceRepository.findAll();
+    }
+
+    @GetMapping("/{categorieServiceId}")
+    public Optional<CategorieService> getCategorieServiceById(@PathVariable Long categorieServiceId){
+        return categorieServiceRepository.findById(categorieServiceId);
+    }
+
+    @PostMapping
+    public CategorieService addCategorieService(@RequestBody CategorieService categorieService){
+        return categorieServiceRepository.save(categorieService);
+    }
+
+    @PutMapping("/categorieServiceId")
+    public void updateEleve(@RequestBody Eleve categorieService, @PathVariable Long categorieServiceId){
+        categorieServiceRepository.findById(categorieServiceId).map(categorieService1 -> {
+
+            return categorieServiceRepository.save(categorieService1);
+        });
+    }
+
+    @DeleteMapping("/categorieServiceId")
+    public void deleteCategorieService(@PathVariable Long categorieServiceId){
+        categorieServiceRepository.deleteById(categorieServiceId);
+    }
+}
